c5197bcb14eda72042347f8f9f7428a8eb69763b
kamarmack <kamarmack112@gmail.com>
2022-04-03 16:36:41 -0500

    implement fs writes

---
+++ src/utils/functions/fs/writes.test.ts
@@ -0,0 +1,43 @@
+import { promises as fs_async } from 'fs';
+// eslint-disable-next-line node/no-unpublished-import
+import { v4 as uuid } from 'uuid';
+import writeStrToFile from './writes';
+import { getCwd } from './path';
+import { logger } from '../console/logger';
+import exec from '../node/exec';
+
+test('utils > functions > fs > writes', function () {
+	// Data
+	const path = `${getCwd()}/tests/writes.test.txt`;
+	const str = `uuid: ${uuid()}; timestamp: ${new Date().toISOString()}`;
+	// Tasks
+	async function cleanup(): Promise<void> {
+		try {
+			if (await fs_async.stat(path)) {
+				logger.log({ msg: `cleaning up ${path}`, debug: false });
+				await fs_async.rm(path);
+			}
+		} catch {
+			// eslint-disable-next-line no-empty
+		}
+	}
+	async function write(): Promise<void> {
+		await writeStrToFile({ path, str });
+	}
+	async function stat(): Promise<void> {
+		const { stdout } = await exec(`cat ${path}`);
+		const valid_write = stdout.includes(str);
+		type T = typeof valid_write;
+		expect<T>(valid_write).toBe<T>(true);
+	}
+	// Tests
+	return (async function () {
+		await [cleanup, write, stat]
+			.map(function (task) {
+				return async function () {
+					await task();
+				};
+			})
+			.reduce((acc, fn) => acc.then(fn), Promise.resolve());
+	})();
+});
--- src/utils/functions/fs/writes.ts
+++ src/utils/functions/fs/writes.ts
@@ -1,1 +1,17 @@
-// Todo
+import { logger, type LoggerFnOptions } from '../console/logger';
+import { promises as fs_async } from 'fs';
+import { PathFnParams } from './path';
+const { writeFile } = fs_async;
+
+export type WriteStrToFileParams = PathFnParams & { str: string };
+async function writeStrToFile(
+	params: WriteStrToFileParams & LoggerFnOptions,
+): Promise<void> {
+	try {
+		await writeFile(params.path, params.str);
+	} catch (msg) {
+		logger.error({ msg, ...params });
+		throw new Error();
+	}
+}
+export default writeStrToFile;
