ed403d5f63689b5e05ee5eb3d4c1e31223a10011
kamarmack <kamarmack112@gmail.com>
2022-04-02 21:37:01 -0500

    improve the sets tests using type assertions

--- src/utils/ts/sets.test.ts
+++ src/utils/ts/sets.test.ts
@@ -1,21 +1,56 @@
 import { O } from './sets';
 
-test('utils > ts > sets > O', function () {
+test('utils > ts > sets > O > objects', function () {
+	// Data
 	const foo = {
 		b: null,
 		a: null,
 		r: null,
 	} as const;
 	const foo_keys = O.keys(foo);
-	// Contains
-	expect(foo_keys).toContain('b');
-	expect(foo_keys).toContain('a');
-	expect(foo_keys).toContain('r');
-	// Length
-	expect(foo_keys).toHaveLength(3);
-	// Order
-	const foo_keys_explicit = ['b', 'a', 'r'];
-	expect(foo_keys).toStrictEqual(foo_keys_explicit);
-	const foo_keys_wrong_order = ['r', 'b', 'a'];
-	expect(foo_keys).not.toStrictEqual(foo_keys_wrong_order);
+	// Types
+	type T = typeof foo_keys;
+	type K = T[number];
+	// Tests: Contains
+	expect<T>(foo_keys).toContain<K>('b');
+	expect<T>(foo_keys).toContain<K>('a');
+	expect<T>(foo_keys).toContain<K>('r');
+	// Tests: Length
+	expect<T>(foo_keys).toHaveLength(3);
+	// Tests: Order
+	const foo_keys_in_order: T = ['b', 'a', 'r'];
+	const foo_keys_wrong_order: T = ['r', 'b', 'a'];
+	expect<T>(foo_keys).toStrictEqual<T>(foo_keys_in_order);
+	expect<T>(foo_keys).not.toStrictEqual<T>(foo_keys_wrong_order);
+});
+
+test('utils > ts > sets > O > enums', function () {
+	// Data
+	enum Foo {
+		b = 'b',
+		a = 'a',
+		r = 'r',
+	}
+	const foo_keys = O.keys(Foo);
+	// Types
+	type T = typeof foo_keys;
+	type K = T[number];
+	// Tests: Contains
+	expect<T>(foo_keys).toContain<K>(Foo.b);
+	expect<T>(foo_keys).toContain<K>('b');
+	expect<T>(foo_keys).toContain<K>(Foo.a);
+	expect<T>(foo_keys).toContain<K>('a');
+	expect<T>(foo_keys).toContain<K>(Foo.r);
+	expect<T>(foo_keys).toContain<K>('r');
+	// Tests: Length
+	expect<T>(foo_keys).toHaveLength(3);
+	// Tests: Order
+	const foo_keys_in_order: T = [Foo.b, Foo.a, Foo.r];
+	const foo_keys_exp_in_order: T = ['b', 'a', 'r'];
+	const foo_keys_wrong_order: T = [Foo.r, Foo.b, Foo.a];
+	const foo_keys_exp_wrong_order: T = ['r', 'b', 'a'];
+	expect<T>(foo_keys).toStrictEqual<T>(foo_keys_in_order);
+	expect<T>(foo_keys).toStrictEqual<T>(foo_keys_exp_in_order);
+	expect<T>(foo_keys).not.toStrictEqual<T>(foo_keys_wrong_order);
+	expect<T>(foo_keys).not.toStrictEqual<T>(foo_keys_exp_wrong_order);
 });
