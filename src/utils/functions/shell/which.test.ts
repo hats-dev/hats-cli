926f033404adb5a162064f556d108d6cb64c07c5
kamarmack <kamarmack112@gmail.com>
2022-04-05 15:38:00 -0500

    refactor which to include batch methods

--- src/utils/functions/shell/which.test.ts
+++ src/utils/functions/shell/which.test.ts
@@ -1,15 +1,68 @@
-import { whichFile } from './which';
+import {
+	hasRequiredPrograms,
+	LocalProgramType,
+	whichProgram,
+	whichPrograms,
+} from './which';
 
-test('utils > functions > shell > which', function () {
-	return (async function () {
-		// Data
-		const npm_file = await whichFile({ program: 'npm' });
-		const fake_file = await whichFile({ program: 'fake-shell-program' });
-		// Types
-		type T = typeof npm_file;
-		type Tf = typeof fake_file;
-		// Tests
-		expect<T>(npm_file).toBe<T>(true);
-		expect<Tf>(fake_file).toBe<T>(false);
-	})();
+describe('utils > functions > shell > which', function () {
+	const fake_shell_program =
+		'fake-shell-program' as unknown as LocalProgramType;
+	const valid_programs = [LocalProgramType.npm, LocalProgramType.node];
+	const fake_programs = [...valid_programs, fake_shell_program];
+	test('whichProgram', function () {
+		return (async function () {
+			// Data
+			const which_npm = await whichProgram({ program: LocalProgramType.npm });
+			const which_fake = await whichProgram({
+				program: fake_shell_program,
+			});
+			// Types
+			type T = typeof which_npm;
+			type Tf = typeof which_fake;
+			// Tests
+			expect<T>(which_npm).toBe<T>(true);
+			expect<Tf>(which_fake).toBe<T>(false);
+		})();
+	});
+	test('whichPrograms', function () {
+		return (async function () {
+			// Data
+			const which_valid_programs = await whichPrograms({
+				programs: valid_programs,
+			});
+			const which_fake_programs = await whichPrograms({
+				programs: fake_programs,
+			});
+			// Types
+			type T = typeof which_valid_programs;
+			type Tf = typeof which_fake_programs;
+			type K = LocalProgramType;
+			// Tests
+			expect<T>(which_valid_programs).toHaveLength(2);
+			expect<T>(which_valid_programs).toContain<K>(LocalProgramType.npm);
+			expect<T>(which_valid_programs).toContain<K>(LocalProgramType.node);
+			expect<Tf>(which_fake_programs).toHaveLength(2);
+			expect<Tf>(which_fake_programs).toContain<K>(LocalProgramType.npm);
+			expect<Tf>(which_fake_programs).toContain<K>(LocalProgramType.node);
+			expect<Tf>(which_fake_programs).not.toContain<K>(fake_shell_program);
+		})();
+	});
+	test('hasRequiredPrograms', function () {
+		return (async function () {
+			// Data
+			const installed_programs = await hasRequiredPrograms({
+				programs: valid_programs,
+			});
+			const missing_programs = await hasRequiredPrograms({
+				programs: fake_programs,
+			});
+			// Types
+			type T = typeof installed_programs;
+			type Tf = typeof missing_programs;
+			// Tests
+			expect<T>(installed_programs).toBe<T>(true);
+			expect<Tf>(missing_programs).toBe<T>(false);
+		})();
+	});
 });
--- src/utils/functions/shell/which.ts
+++ src/utils/functions/shell/which.ts
@@ -1,8 +1,19 @@
+import { O } from '../../ts/sets';
 import { LoggerFnOptions, logger } from '../console/logger';
 import exec from '../node/exec';
 
-type WhichParams = { program: string } & LoggerFnOptions;
-export async function whichFile(params: WhichParams): Promise<boolean> {
+export enum LocalProgramType {
+	'auto-changelog' = 'auto-changelog',
+	doctoc = 'doctoc',
+	gh = 'gh',
+	git = 'git',
+	node = 'node',
+	npm = 'npm',
+}
+export const local_program_types = O.keys(LocalProgramType);
+
+type WhichParams = { program: LocalProgramType } & LoggerFnOptions;
+export async function whichProgram(params: WhichParams): Promise<boolean> {
 	try {
 		const { stdout } = await exec(`which ${params.program}`);
 		return !!stdout;
@@ -13,5 +24,35 @@ export async function whichFile(params: WhichParams): Promise<boolean> {
 			...params,
 		});
 		return false;
+	}
+}
+
+type WhichProgramsParams = {
+	readonly programs: LocalProgramType[];
+} & LoggerFnOptions;
+export async function whichPrograms(
+	params: WhichProgramsParams,
+): Promise<LocalProgramType[]> {
+	try {
+		const which_programs = await Promise.all(
+			params.programs.slice().map((program) => whichProgram({ program })),
+		);
+		return params.programs.slice().filter((_, i) => which_programs[i]);
+	} catch (msg) {
+		logger.error({ msg, ...params });
+		throw new Error();
+	}
+}
+
+type HasRequiredProgramsParams = WhichProgramsParams;
+export async function hasRequiredPrograms(
+	params: HasRequiredProgramsParams,
+): Promise<boolean> {
+	try {
+		const which_programs = await whichPrograms(params);
+		return params.programs.length === which_programs.length;
+	} catch (msg) {
+		logger.error({ msg, ...params });
+		throw new Error();
 	}
 }
