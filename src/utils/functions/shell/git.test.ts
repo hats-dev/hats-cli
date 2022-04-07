a67f2514126e67b72d82018e6d92cee4e99fc11b
kamarmack <kamarmack112@gmail.com>
2022-04-06 20:55:27 -0500

    remove git config reader from shell functions

--- src/utils/functions/shell/git.test.ts
+++ src/utils/functions/shell/git.test.ts
@@ -1,6 +1,5 @@
-import { logger } from '../console/logger';
 import { getCwd } from '../fs/path';
-import { getGitUserConfigs, isGitTracked } from './git';
+import { isGitTracked } from './git';
 
 describe('utils > functions > shell > git', function () {
 	test('isGitTracked', function () {
@@ -8,22 +7,6 @@ describe('utils > functions > shell > git', function () {
 			const is_git_tracked = await isGitTracked({ path: getCwd() });
 			type T = typeof is_git_tracked;
 			expect<T>(is_git_tracked).toBe<T>(true);
-		})();
-	});
-	test('getGitUserConfigs', function () {
-		return (async function () {
-			const git_user_configs = await getGitUserConfigs();
-			type T = typeof git_user_configs;
-			expect<T>(git_user_configs).toMatchObject(
-				expect.objectContaining<T>({
-					user: {
-						name: expect.stringContaining('') as string,
-						email: expect.stringContaining('') as string,
-						username: expect.stringContaining('') as string,
-					},
-				}),
-			);
-			logger.log({ msg: git_user_configs, debug: false });
 		})();
 	});
 });
--- src/utils/functions/shell/git.ts
+++ src/utils/functions/shell/git.ts
@@ -1,7 +1,6 @@
 import { PathFnParams } from '../fs/path';
 import exec from '../node/exec';
 import { logger, type LoggerFnOptions } from '../console/logger';
-import { EmptyObject } from '../../ts/objects';
 
 export type IsGitTrackedParams = PathFnParams;
 export async function isGitTracked(
@@ -11,51 +10,6 @@ export async function isGitTracked(
 		const is_git_tracked_sh = `git -C ${params.path} rev-parse 2>/dev/null; echo $?`;
 		const { stdout } = await exec(is_git_tracked_sh);
 		return stdout.replace(/\n/g, '') === '0';
-	} catch (msg) {
-		logger.error({ msg, ...params });
-		throw new Error();
-	}
-}
-
-type GitUserConfig = {
-	user?: {
-		name?: string;
-		email?: string;
-		username?: string;
-	};
-};
-type UnknownGitConfig = Record<string, Record<string, string>>;
-export type GetGitUserConfigsParams = typeof EmptyObject;
-export async function getGitUserConfigs(
-	params: GetGitUserConfigsParams & LoggerFnOptions = {},
-): Promise<GitUserConfig> {
-	try {
-		const get_git_user_configs_sh = 'git config --list --global';
-		const { stdout } = await exec(get_git_user_configs_sh);
-		const configs = stdout.split('\n').filter((config) => config);
-		return configs.reduce(function (acc: UnknownGitConfig, config) {
-			const arr = config.split('=');
-			const key_segments = arr[0] || '';
-			const value = arr[1] || '';
-			const key_arr = key_segments.split('.');
-			const key_root = key_arr[0] || '';
-			const key_path = key_arr.slice(1).join('') || '';
-			if (!acc[key_root]) {
-				return {
-					...acc,
-					[key_root]: {
-						[key_path]: value,
-					},
-				};
-			}
-			return {
-				...acc,
-				[key_root]: {
-					...acc[key_root],
-					[key_path]: value,
-				},
-			};
-		}, {});
 	} catch (msg) {
 		logger.error({ msg, ...params });
 		throw new Error();
