4e6f8e3933625668639e588c3735d0963c20ad5a
kamarmack <kamarmack112@gmail.com>
2022-04-07 07:42:02 -0500

    add functional exact type helper for obj spreads

--- src/utils/ts/objects.ts
+++ src/utils/ts/objects.ts
@@ -1,1 +1,7 @@
 export const EmptyObject = {} as const;
+
+type ExactType<T, U> = T extends U ? (U extends T ? T : never) : never;
+export const exact =
+	<E>() =>
+	<T>(t: ExactType<T, E>) =>
+		t;
